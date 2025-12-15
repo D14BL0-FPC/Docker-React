fe28ad7a834e6090c785eefcebd7f4b9e3501b20
francisco <FranciscoParraCaparros2@hotmail.com>
2025-12-15 19:37:10 +0100

    Añadimos comentarios al segundo plano y añadido de nginx

---
+++ docker/docker-react/dockerfiles/nginx/admin/start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+config_nginx() {
+    nginx &
+}
+
+load_entrypoint_base(){
+    bash /root/admin/base/start.sh
+}
+
+certificados_ssl(){
+
+    mkdir -p /etc/nginx/certs
+    cp -r /root/admin/nginx/cert/* /etc/nginx/certs/
+
+}
+
+main(){
+ load_entrypoint_base
+ certificados_ssl
+ config_nginx
+    # Mantener el contenedor en ejecución
+    tail -f /dev/null
+}
+
+main
